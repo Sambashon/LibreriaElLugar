d4bc9b5776d2703bb0292c0c0ba00fdaf9a5b7a8
Seba <sebastianafuenmayord@gmail.com>
2026-04-12 15:58:15 -0300

    Revamp to landing page #inicio

--- Public/src/js/index.js
+++ Public/src/js/index.js
@@ -14,3 +14,87 @@ window.addEventListener("scroll", function(){
     }
     prevScroll = currentScroll;
 });
+
+
+const row = document.getElementById("row");
+
+const rows = document.querySelectorAll("#row");
+
+let autoScrollInterval;
+let resumeTimeout;
+
+function startAutoScroll() {
+    autoScrollInterval = setInterval(() => {
+        rows.forEach(row => {
+            row.scrollBy({
+                left: 250,
+                behavior: "smooth"
+            });
+        });
+        
+
+        // loop infinito
+        if (row.scrollLeft + row.clientWidth >= row.scrollWidth - 5) {
+            row.scrollTo({ left: 0, behavior: "smooth" });
+        }
+    }, 3500);
+}
+
+function stopAutoScroll() {
+    clearInterval(autoScrollInterval);
+}
+
+function resetAutoScroll() {
+    stopAutoScroll();
+    clearTimeout(resumeTimeout);
+
+    resumeTimeout = setTimeout(() => {
+        startAutoScroll();
+    }, 10000); // 10 segundos
+}
+
+function manualScroll(amount) {
+    row.scrollBy({
+        left: amount,
+        behavior: "smooth"
+    });
+    resetAutoScroll();
+}
+
+// Detectar interacción del usuario
+row.addEventListener("mousedown", resetAutoScroll);
+row.addEventListener("touchstart", resetAutoScroll);
+
+// iniciar
+startAutoScroll();
+
+const cifras = document.querySelectorAll(".cifra");
+
+function removeChars(cifra){
+    const str = cifra.innerText;
+    const cleaned = str.replace(/[^a-zA-Z0-9]/g, ""); 
+    return parseInt(cleaned);
+}
+
+function countUp(cifra){
+    const original = cifra.innerText;
+    const target = removeChars(cifra);
+    let current = 0;
+
+    const interval = setInterval(() => {
+
+        current += (target - current) * 0.05; 
+
+        if (Math.abs(target - current) < 1) {
+            cifra.innerText = original;
+            clearInterval(interval);
+        } else {
+            cifra.innerText = Math.floor(current);
+        }
+
+    }, 20);
+}
+
+cifras.forEach(cifra=> {
+    countUp(cifra);
+})
