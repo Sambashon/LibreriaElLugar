0d29d07741547dc900a1de914c8b9ed2e60301f4
Sebastian <sebastianafuenmayord@gmail.com>
2026-06-17 13:24:35 -0300

    Added icon to header in catalogue.php

--- docs/catalogue.php
+++ docs/catalogue.php
@@ -17,6 +17,7 @@ $initialSearch = isset($_GET['search']) ? htmlspecialchars($_GET['search'], ENT_
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catálogo | El lugar</title>
+    <link rel="icon" href="Resources/logos/libreriasimple.jpg" type="image/png">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
